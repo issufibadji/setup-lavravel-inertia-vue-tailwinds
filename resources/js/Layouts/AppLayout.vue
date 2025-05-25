6e47c1403e24a15369f6f850ab8794db9dfe28a0
Issufi Badji <issufibadji@gmail.com>
2025-05-24 21:03:35 -0300

    FEATURE:primeiro comite de setup laravel+inertia+vue+tailwinds Css

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <header class="bg-white shadow dark:bg-gray-800">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-xl font-bold">
+        <slot name="header" />
+      </div>
+    </header>
+    <main class="p-4">
+      <slot />
+    </main>
+  </div>
+</template>
